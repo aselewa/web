07f9a8c39d9082f9757fa4e1c6b5d54f59762285
Alan Selewa <selewa@uchicago.edu>
2017-10-02 21:33:38 -0500

    updated content

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("~/web/")
+setwd("/Users/aselewa/web/")
 
 #render your sweet site. 
 rmarkdown::render_site()
